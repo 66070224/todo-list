f27e8954595e38d194376c3478068efecf4f9d15
66070224 <66070224@kmitl.ac.th>
2026-02-23 22:22:48 +0700

    faster dashboard page

--- components/CardSection.tsx
+++ components/CardSection.tsx
@@ -3,12 +3,24 @@ import { prisma } from "@/prisma";
 import AllTasksPieChart from "./AllTasksPieChart";
 import CategoryPieChart from "./CategoryPieChar";
 import { TaskCategory } from "@/app/generated/prisma/enums";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default async function CardSection() {
   const session = await auth();
   if (!session?.user?.id) return;
 
-  const allCategoryTasks = await prisma.task.groupBy({
+  const grouped = await prisma.task.groupBy({
+    by: ["category", "status"],
+    where: {
+      userId: session.user.id,
+    },
+    _count: {
+      _all: true,
+    },
+  });
+
+  const categorySummary = await prisma.task.groupBy({
     by: ["category"],
     where: {
       userId: session.user.id,
@@ -18,46 +30,31 @@ export default async function CardSection() {
     },
   });
 
-  const allStatusTasks = async (userId: string, category: TaskCategory) =>
-    await prisma.task.groupBy({
-      by: ["category", "status"],
-      where: {
-        userId,
-        category,
-      },
-      _count: {
-        _all: true,
-      },
-    });
+  const byCategory = grouped.reduce(
+    (acc, curr) => {
+      if (!acc[curr.category]) acc[curr.category] = [];
+      acc[curr.category].push(curr);
+      return acc;
+    },
+    {} as Record<string, typeof grouped>,
+  );
 
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      {/* ALL */}
-      <AllTasksPieChart data={allCategoryTasks} />
-      <CategoryPieChart
-        data={await allStatusTasks(session.user.id, "PERSONAL")}
-        category="Personal"
-      />
-      <CategoryPieChart
-        data={await allStatusTasks(session.user.id, "FAMILY")}
-        category="Family"
-      />
-      <CategoryPieChart
-        data={await allStatusTasks(session.user.id, "STUDY")}
-        category="Study"
-      />
-      <CategoryPieChart
-        data={await allStatusTasks(session.user.id, "WORK")}
-        category="Work"
-      />
-      <CategoryPieChart
-        data={await allStatusTasks(session.user.id, "HEALTH")}
-        category="Health"
-      />
-      <CategoryPieChart
-        data={await allStatusTasks(session.user.id, "FINANCE")}
-        category="Finance"
-      />
+      {grouped.length !== 0 ? (
+        <>
+          <AllTasksPieChart data={categorySummary} />
+          {Object.entries(byCategory).map(([category, data]) => (
+            <CategoryPieChart key={category} data={data} category={category} />
+          ))}{" "}
+        </>
+      ) : (
+        <div>
+          <Button asChild>
+            <Link href={"/tasks"}>At your first task</Link>
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
